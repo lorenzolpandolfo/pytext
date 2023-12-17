f2f3e9bdc52726034cc8125b2552bbef37037a7e
Lorenzo Pandolfo <133379234+lorenzolpandolfo@users.noreply.github.com>
2023-12-17 13:15:50 -0300

    gui update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
+from tkinter import font
 import customtkinter as ctk
-import random
+
 
 class MainApp:
     def __init__(self, root):
@@ -11,22 +12,58 @@ class MainApp:
 
 
     def create_window(self):
-        root.geometry("740x780")
+        root.geometry("1100x780")
         self.root.title("The Pytext Editor")
     
 
     def create_frames(self):
-        # creating the main frame
-        self.mainframe = ctk.CTkFrame(root, width=100)
-        self.mainframe.pack(expand=True, fill="both")
+        # creating left frame
+        self.leftframe = ctk.CTkFrame(root)
+        self.leftframe.grid(row=0, column=0, sticky="ns")
+        # peso 0 para não expandir (janela)
+        self.root.columnconfigure(0, weight=0)
 
-        # creating the left label
-        teste = ctk.CTkLabel(self.mainframe, width=5)
-        teste.pack(side=tk.LEFT, expand=False, fill='both')
+        # creating the main frame
+        self.mainframe = ctk.CTkFrame(root)
+        self.mainframe.grid(row=0, column=1, sticky="nsew")
+        # peso 1 para que ele expanda (janela)
+        self.root.columnconfigure(1, weight=1)
+        self.root.rowconfigure(0, weight=1)
+
+        # creating bottom frame
+        self.bottomframe = ctk.CTkFrame(root)
+        self.bottomframe.grid(row=1, column=0, columnspan=2, sticky="ew")
+
 
     def create_widgets(self):
-        self.textarea = ctk.CTkTextbox(self.mainframe, width=90, height=90, wrap=ctk.WORD)
-        self.textarea.pack(expand=True, fill="both", padx=10, pady=(20,10))
+        # initializing firacode font
+        src = r"fonts\firacode.ttf"
+        # carrega a fonte
+        ctk.FontManager.load_font(src)
+        # agora ele reconhece a family Fira Code, porque eu carreguei antes
+        firacode = ctk.CTkFont(family="Fira Code", size=19) 
+
+        # initializing main text area
+        self.main_textarea = ctk.CTkTextbox(self.mainframe, wrap=ctk.WORD, font=firacode)
+        self.main_textarea.grid(row=0, column=0, sticky="nsew", padx=10, pady=(20, 10))
+        # configurando o mainframe
+        self.mainframe.columnconfigure(0, weight=1)
+        self.mainframe.rowconfigure(0, weight=1)
+
+        # initializing left text area
+        self.left_textarea = ctk.CTkTextbox(self.leftframe, width=50, wrap=ctk.CHAR, font=firacode)
+        self.left_textarea.grid(row=0, column=0, sticky="ns", padx=(10,10), pady=(20,10))
+        # configurando o leftframe
+        self.leftframe.columnconfigure(0, weight=1)
+        self.leftframe.rowconfigure(0, weight=1)
+
+        # creating the bottom label
+        bottom_output_mode = ctk.CTkLabel(self.bottomframe, text="view mode", justify="center", font=firacode)
+        bottom_output_mode.grid(row=1, column=0, sticky="ew")
+        # configurando o bottomframe
+        self.bottomframe.columnconfigure(0, weight=1)
+        self.bottomframe.rowconfigure(0, weight=1)
+
     
     
 
