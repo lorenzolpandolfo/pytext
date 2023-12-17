d7452d7e3ba5f730d53aa84e7bfd865318d2f80d
Lorenzo Pandolfo <133379234+lorenzolpandolfo@users.noreply.github.com>
2023-12-17 01:06:09 -0300

    general update

--- main.py
+++ main.py
@@ -5,38 +5,30 @@ import random
 class MainApp:
     def __init__(self, root):
         self.root = root
-        self.window()
+        self.create_window()
         self.create_frames()
         self.create_widgets()
-        self.bind()
 
 
-    def resize(self, w):
-        self.mainframe.configure(width = w)
-        self.root.update()
-
-    def bind(self):
-        # Com essa função, eu consigo realizar uma ação por movimento da janela, resize, alt+tab nela, etc
-        # <Configure> é mudanças na janela. <Button-1> seria o mouse1, por exemplo
-        # Sem o lambda e, ele não roda a função varias vezes
-        root.bind("<Configure>", lambda : self.resize())
-
-    def window(self):
+    def create_window(self):
         root.geometry("740x780")
-        self.root.title("Pytext")
+        self.root.title("The Pytext Editor")
     
 
     def create_frames(self):
+        # creating the main frame
         self.mainframe = ctk.CTkFrame(root, width=100)
-        self.mainframe.pack(expand=True)
+        self.mainframe.pack(expand=True, fill="both")
 
+        # creating the left label
+        teste = ctk.CTkLabel(self.mainframe, width=5)
+        teste.pack(side=tk.LEFT, expand=False, fill='both')
 
     def create_widgets(self):
-        self.textarea = ctk.CTkTextbox(self.mainframe, width=90, height=90)
-        self.textarea.pack(expand=True, padx=10, pady=(20,10))
+        self.textarea = ctk.CTkTextbox(self.mainframe, width=90, height=90, wrap=ctk.WORD)
+        self.textarea.pack(expand=True, fill="both", padx=10, pady=(20,10))
     
     
-
 
 if __name__ == "__main__":
     root = ctk.CTk()
