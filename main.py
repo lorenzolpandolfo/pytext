251d410d65ec34b242b8498d9868f37fd8c3214b
Lorenzo Pandolfo <133379234+lorenzolpandolfo@users.noreply.github.com>
2023-12-17 14:22:24 -0300

    general update

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import customtkinter as ctk
 
 
 class MainApp:
-    def __init__(self, root, modo = "view"):
+    def __init__(self, root):
         self.root = root
-        self.modo = modo
+        self.modo = "view"
         self.create_window()
         self.create_frames()
         self.create_widgets()
         self.capture_commands()
+        # por algum motivo ele buga a primeira mudança de modo, entao ele força a mudança no começo
+        self.trocar_modo(self.modo)
 
 
     def create_window(self):
@@ -48,6 +50,8 @@ class MainApp:
         # initializing main text area
         self.main_textarea = ctk.CTkTextbox(self.mainframe, wrap=ctk.WORD, font=firacode)
         self.main_textarea.grid(row=0, column=0, sticky="nsew", padx=10, pady=(20, 10))
+        self.main_textarea.focus_set()
+
         # configurando o mainframe
         self.mainframe.columnconfigure(0, weight=1)
         self.mainframe.rowconfigure(0, weight=1)
@@ -60,37 +64,54 @@ class MainApp:
         self.leftframe.rowconfigure(0, weight=1)
 
         # creating the bottom label
-        bottom_output_mode = ctk.CTkLabel(self.bottomframe, text="view mode", justify="center", font=firacode)
-        bottom_output_mode.grid(row=1, column=0, sticky="ew")
-        # configurando o bottomframe
-        self.bottomframe.columnconfigure(0, weight=1)
-        self.bottomframe.rowconfigure(0, weight=1)
+        self.bottom_output_mode = ctk.CTkLabel(self.bottomframe, text=self.modo, justify="center", font=firacode)
+        self.bottom_output_mode.grid(row=1, column=0, sticky="ew", columnspan=2)
+
+        # Criando o textbox no segundo grid
+        self.bottom_command_output = ctk.CTkTextbox(self.bottomframe, font=firacode, width=100, height=2)
+        # Como o sticky é "e", ele vai ser ancorado para o leste ->
+        self.bottom_command_output.grid(row=1, column=1, sticky="e", padx=(0, 10), pady=(0, 5))
+
+        # o peso é 1 para que ele se mova para a direita
+        self.bottomframe.columnconfigure(1, weight=1)
+        # Configurando o textbox para ser menor verticalmente
+        self.bottom_command_output.rowconfigure(1, weight=0)  # Ajuste para tornar a segunda linha menor
 
 
     def capture_commands(self):
         # ele envia argumentos mesmo sem indicar
-        root.bind("<Key>", self.teste)
+        root.bind("<Key>", self.checar_se_precisa_atualizar_contador)
+        root.bind("<Button-1>", self.atualizar_contador)
         root.bind("<Escape>", lambda e: self.trocar_modo(self.modo))
     
 
     def trocar_modo(self, modo):
         if modo == "view":
             self.modo = "insert"
+            self.main_textarea.configure(state="disabled")
         else:
             self.modo = "view"
+            self.main_textarea.configure(state="normal")
         
-        print(self.modo)
+        return self.bottom_output_mode.configure(text=modo)
 
-    def teste(self, event):
+    def checar_se_precisa_atualizar_contador(self, event):
         tecla = event.keysym
+        print(event)
 
         match tecla:
-            case "Up"| "Down"| "Left"| "Right"| "Return" |"BackSpace":
-                print("necessita atualizar contador! - ", event.keysym)
+            case "Up"| "Down"| "Left"| "Right"| "Return" |"BackSpace" |"Button-1":
+                print("necessita atualizar contador! - ", tecla)
+                return self.atualizar_contador()
             case _:
                 return 0
 
+
+    def atualizar_contador(self, event = None):
+        print("contador atualizado!", event)
+        return 0
+
 if __name__ == "__main__":
     root = ctk.CTk()
-    app = MainApp(root, "view")
+    app = MainApp(root)
     root.mainloop()
