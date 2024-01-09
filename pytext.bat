e6d9625be7a5e5e7134331b8f8071fbb72cb114c
Lorenzo Pandolfo <133379234+lorenzolpandolfo@users.noreply.github.com>
2024-01-09 19:24:16 -0300

    pytext.bat modified

--- pytext.bat
+++ pytext.bat
@@ -1,2 +1,7 @@
 @echo off
-python "%ProgramFiles%\pytext\main.py" %*
+
+rem set the pytext main.py directory to yours. Feel free to change it.
+rem the default directory is: (your user)\Documents\pytext\main.py
+set pytext-main-dir="%UserProfile%\Documents\pytext\main.py"
+
+python %pytext-main-dir% %*
