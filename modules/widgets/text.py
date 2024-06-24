36db2a6aa2da1f17fbbc05397ef76488951a1afa
Lorenzo Pandolfo <lorenzopandolfo2004@gmail.com>
2024-06-24 11:50:43 -0300

    code cleaning and general improvements

--- modules/widgets/text.py
+++ modules/widgets/text.py
@@ -3,24 +3,22 @@ import os
 
 from modules.FileManager     import FileManager
 from modules.tklinenums      import TkLineNumbers
-from modules.SyntaxColors    import SyntaxColors
 from modules.Application     import Application
 from modules.TextUtils       import TextUtils
 from modules.LanguageManager import LanguageManager
-
-import pygments
-from pygments.lexers import get_lexer_by_name
 
 
 class Generaltext(Text):
     """ Includes methods that Maintext and Lefttext use. """
     def __init__(self, master, *args, **kwargs):
         super().__init__(master, *args, **kwargs, insertofftime=0)
-        self.exp_curdir_color = None
-        self.exp_file_color = None
-        self.exp_dir_color = None
-        self.selected_line_color = None
-        self.path = None
+        self.font_color             : str = ''
+        self.bg_color               : str = ''
+        self.exp_curdir_color       : str = ''
+        self.exp_file_color         : str = ''
+        self.exp_dir_color          : str = ''
+        self.selected_line_color    : str = ''
+        self.path                   : str = ''
 
         self.sys_theme = master.sys_theme
         self.theme = master.theme
@@ -31,41 +29,33 @@ class Generaltext(Text):
         dark = "_dark" if self.sys_theme == "dark" else ""
         cursor_color = self.theme[f"cursor_color{dark}"]
         self.configure(
-            bd=0, highlightthickness=0,
-            blockcursor=True, insertbackground=cursor_color, insertunfocussed="hollow")
-
+            bd=0, highlightthickness=0, blockcursor=True,
+            insertbackground=cursor_color, insertunfocussed="hollow")
 
     def enable_auto_highlight_line(self):
-        self.bind("<Key>", lambda _: self.after_idle(self.highlight_selected_line))
+        self.bind("<Key>",      lambda _: self.after_idle(self.highlight_selected_line))
         self.bind("<Button-1>", lambda _: self.after_idle(self.highlight_selected_line))
 
-    def highlight_selected_line(self, event=None):
+    def highlight_selected_line(self):
         line_start = int(self.index("insert").split(".")[0])
         self.tag_remove("current_line_color", "1.0", "end")
         self.tag_add("current_line_color", f"{line_start}.0", f"{line_start + 1}.0")
         self.tag_config("current_line_color", background=self.selected_line_color)
 
-    def load_theme(self, child):
+    def load_theme(self, widget):
         dark = "_dark" if self.sys_theme == "dark" else ""
-        child = str(child)
-
-        widget = "main_textbox" if "maintext" in child else "left_textbox"
-
-        bg_color            = self.theme["widgets"][widget][f"bg{dark}"]
-        selected_line_color = self.theme["widgets"][widget][f"selected_line{dark}"]
-        font_color          = self.theme["widgets"][widget][f"font{dark}"]
-        exp_dir_color       = self.theme["explorer"][f"dir{dark}"]
-        exp_file_color      = self.theme["explorer"][f"file{dark}"]
-        exp_curdir_color    = self.theme["explorer"][f"curdir{dark}"]
-
-        return bg_color, selected_line_color, font_color, exp_dir_color, exp_file_color, exp_curdir_color
+        self.bg_color            = self.theme["widgets"][widget][f"bg{dark}"]
+        self.selected_line_color = self.theme["widgets"][widget][f"selected_line{dark}"]
+        self.font_color          = self.theme["widgets"][widget][f"font{dark}"]
+        self.exp_dir_color       = self.theme["explorer"][f"dir{dark}"]
+        self.exp_file_color      = self.theme["explorer"][f"file{dark}"]
+        self.exp_curdir_color    = self.theme["explorer"][f"curdir{dark}"]
 
     def write_file_content(self, content: str | tuple, mark_set: str = "insert"):
         """ Directly write a file content. Used when user opens a file with left sidebar or argv"""
         if not content:
             content = ''
         self.configure(state="normal")
-
         self.delete("1.0", "end")
         self.insert("1.0", content)
 
@@ -115,16 +105,16 @@ class Generaltext(Text):
         Application.set_current_file(full_path)
         return content
 
-    def open_directory(self, dir_path: str, auto_write:bool = True):
+    def open_directory(self, dir_path: str, auto_write: bool = True):
         content = FileManager.open_directory(dir_path)
         if content:
             self.path = dir_path
 
             if auto_write:
-                self.write_directory_content(content, colors=(self.exp_dir_color, self.exp_file_color, self.exp_curdir_color))
-
-    def set_tab_width(self, tabwidth:int):
-        self.configure(tabs=self.cget("font").measure(" ") * tabwidth)
+                self.write_directory_content(
+                    content,
+                    colors=(self.exp_dir_color, self.exp_file_color, self.exp_curdir_color)
+                )
 
     def focus_set(self):
         super().focus_set()
@@ -144,41 +134,31 @@ class Maintext(Generaltext):
         self.sys_theme = master.sys_theme
         self.theme = master.theme
         self.__load_theme__()
-
-        super().enable_auto_highlight_line()
-
-        self._lexer = pygments.lexers.PythonLexer
-        self._colors = SyntaxColors.get_syntax_colors()
         self._enable_binds_()
 
     def __load_theme__(self):
-        (self.bg_color, self.selected_line_color, self.font_color, self.exp_dir_color,
-            self.exp_file_color, self.exp_curdir_color) = super().load_theme(self)
+        super().load_theme("main_textbox")
         self.configure(bg=self.bg_color, foreground=self.font_color, state="disabled")
 
     def _enable_binds_(self):
-        self.bind("<Key>",          lambda e: self.after_idle(self.__bind_dealing__))
+        self.bind("<Key>",          lambda e: self.after_idle(self.__key_dealing__))
         self.bind("<Tab>",          lambda e: TextUtils.__add_tab__(self))
         self.bind("<ISO_Left_Tab>", lambda e: TextUtils.__untab__(self))
         self.bind("<Control-d>",    lambda e: TextUtils.__comment_lines__(self))
 
-    def __bind_dealing__(self):
+    def __key_dealing__(self):
         self.highlight_selected_line()
         self.update_line_counter()
 
     def create_line_counter(self, master):
-        def load_line_counter_theme() -> tuple:
-            dark = "_dark" if self.sys_theme == "dark" else ""
-            bg_color = self.theme["widgets"]["line_counter"][f"bg{dark}"]
-            font_color = self.theme["widgets"]["line_counter"][f"font{dark}"]
-            return bg_color, font_color
-        bg_color, font_color = load_line_counter_theme()
-
+        dark = "_dark" if self.sys_theme == "dark" else ""
+        lc_bg_color = self.theme["widgets"]["line_counter"][f"bg{dark}"]
+        lc_font_color = self.theme["widgets"]["line_counter"][f"font{dark}"]
         tilde_char = Application.mainapp.user_config["nonexistent_char"]
         self._line_counter = TkLineNumbers(
-            master, self, justify="right", colors=(font_color, bg_color), tilde=tilde_char, bd=0
+            master, self, justify="right", colors=(lc_font_color, lc_bg_color), tilde=tilde_char, bd=0
         )
-        self._line_counter.grid(row=0, column=0, sticky="nsew", pady=(0,0))
+        self._line_counter.grid(row=0, column=0, sticky="nsew", pady=(0, 0))
         self.__enable_auto_redraw__()
 
     def __enable_auto_redraw__(self):
@@ -197,8 +177,7 @@ class Lefttext(Generaltext):
     def __init__(self, master, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
         super().enable_auto_highlight_line()
-
-        self.bg_color, self.selected_line_color, self.font_color, self.exp_dir_color, self.exp_file_color, self.exp_curdir_color = super().load_theme(self)
+        super().load_theme("left_textbox")
         self.configure(bg=self.bg_color, state="disabled", border=False)
 
     def updir(self):
